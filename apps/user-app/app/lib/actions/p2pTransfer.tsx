d06a68339538bfcf361f31707977f426d70f961a
Awais Khan <awaisahmedkhan21@gmail.com>
2024-09-25 15:52:58 +0530

    checking CI

--- apps/user-app/app/lib/actions/p2pTransfer.tsx
+++ apps/user-app/app/lib/actions/p2pTransfer.tsx
@@ -40,13 +40,11 @@ export async function P2PTransfer(to: string, amount: number) {
       where: { userId: from },
       data: { amount: { decrement: amount } },
     });
-    console.log("3 complete");
 
     await tx.balance.update({
       where: { userId: toUser?.id },
       data: { amount: { increment: amount } },
     });
-    console.log("1 complete");
 
     await tx.p2pTransfer.create({
       data: {
